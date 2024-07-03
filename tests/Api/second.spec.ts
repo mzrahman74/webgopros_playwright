cc6413d999ccc212838bf8cc3372f5ad92e9270c
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2024-07-03 14:20:50 -0500

    updated

--- tests/Api/second.spec.ts
+++ tests/Api/second.spec.ts
@@ -44,4 +44,29 @@ test.describe.parallel("Api Testing", () => {
     expect(responseBody.id).toBe(4);
     expect(responseBody.token).toBeTruthy();
   });
+  test("unregister", async ({ request }) => {
+    const response = await request.post(`${baseUrl}/register`, {
+      data: {
+        email: "sydney@fife"
+      }
+    });
+    expect(response.status()).toBe(400);
+    const responseBody = JSON.parse(await response.text());
+    expect(responseBody.error).toBe("Missing password");
+  });
+  test("{PUT request - replacement", async ({ request }) => {
+    const response = await request.put(`${baseUrl}/user/2`, {
+      data: {
+        id: 2,
+        name: "fuch rose",
+        year: 2002,
+        color: "#C74375",
+        pantone_value: "17-2032"
+      }
+    });
+    expect(response.status()).toBe(200);
+    const responseBody = JSON.parse(await response.text());
+    console.log(responseBody);
+    expect(responseBody.updatedAt).toBeTruthy();
+  });
 });
