dd310240c9fac1fc79c246c78fab2d326172f430
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2025-01-19 16:50:41 -0600

    asserted video_one for home page

--- tests/webgo.spec.ts
+++ tests/webgo.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from "@playwright/test";
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://webgopros.com");
+});
 
 test("webapp title & screenshot", async ({ page }) => {
-  await page.goto("https://webgopros.com");
   console.log(page.title);
   await expect(page).toHaveURL("https://webgopros.com");
   await expect(page.locator("#id-footer")).toBeVisible();
@@ -15,7 +17,7 @@ test("web confirm about page assertion", async ({ page }) => {
       height: window.innerHeight
     };
   });
-  await page.goto("https://webgopros.com");
+
   if (viewportSize.width <= 991) {
     await page.locator(".navbar-toggler-icon").click();
     await page.locator('a[href="/about"]').click();
@@ -35,7 +37,7 @@ test("web confirm contact page assertion", async ({ page }) => {
       height: window.innerHeight
     };
   });
-  await page.goto("https://webgopros.com");
+
   if (viewportSize.width <= 991) {
     await page.locator(".navbar-toggler-icon").click();
     await page.locator('a[href="/contact"]').click();
@@ -48,4 +50,11 @@ test("web confirm contact page assertion", async ({ page }) => {
   await expect(page.locator("#note-id-3rd")).toBeVisible();
   await expect(page.locator("#id-footer")).toBeVisible();
   await expect(page.locator('footer[id="id-footer" ] p')).toBeVisible();
+});
+
+test("home page assertion", async ({ page }) => {
+  await expect(page).toHaveTitle("Mohammad Rahman Portfolio");
+  const frameLocator = page.locator("iframe[class='video_one']").contentFrame();
+  const locator = frameLocator.owner();
+  await expect(locator).toBeVisible();
 });
